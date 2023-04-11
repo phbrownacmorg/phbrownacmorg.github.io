00429f5d343af353d49b129a5c55616199ee8f41
Peter Brown <phbrown@acm.org>
2023-04-11 15:58:06 -0400

    Validity: regex patterns, custom validity

--- formsdemojs.js
+++ formsdemojs.js
@@ -2,7 +2,8 @@
  * Return true iff the values in formElt are all valid.
  */
 function validForm(formElt) {
-    let valid = somethingChecked('animal');
+    let valid = checkAtLeast('animal', 2);
+    valid = valid && validPhone(formElt.phone);
     return false;
 }
 
@@ -27,12 +28,59 @@ function somethingChecked(parentID) {
         }
     }
     let flag = parentElt.querySelector('.flag');
-    // if (oneIsChecked) {
-    //     flag.classList.add('valid');
-    // }
-    // else {
-    //     flag.classList.remove('valid');
-    // }
     console.log(oneIsChecked);
     return oneIsChecked;   
+}
+
+/**
+ * Return true if at least one of the checkboxes that are children
+ * of the element with id parentID has been checked.
+ */
+function checkAtLeast(parentID, numRequired=1) {
+    let parentElt = document.getElementById(parentID);
+    let boxes = parentElt.querySelectorAll('input[type=checkbox]');
+    //console.log(flag);
+    let numChecked = 0;
+    for (box of boxes) {
+        //console.log(box);
+        if (box.checked) {
+            numChecked = numChecked + 1;
+        }
+
+        // better to have valid on the checkbox than on the flag
+        // Note this could be done by going back through later
+        // and processing *all* the boxes.
+        if (numChecked >= numRequired) {
+            box.classList.add('valid'); 
+        }
+        else {
+            box.classList.remove('valid');
+        }
+    }
+    //let flag = parentElt.querySelector('.flag');
+    console.log('Number checked: ' + numChecked);
+    return (numChecked >= numRequired);   
+}
+
+function validPhone(phoneElt) {
+    // Start by looking at what the HTML already did
+    let valid = phoneElt.validity.valid;
+    // Clear the *custom* validity
+    phoneElt.setCustomValidity("");
+
+    // Now, look for specific invalid inputs
+    let value = phoneElt.value;
+    if (value.substring(1, 3) == '11') {
+        console.log('x11 area code');
+        valid = false;
+        phoneElt.setCustomValidity('The area code cannot have the form N11');
+    }
+    else if (value.substring(0,10) == '800-555-01') {
+        console.log('Fictional phone number');
+        valid = false;
+        phoneElt.setCustomValidity('800-555-01NN is for fictional phone numbers.');
+    }
+    
+    console.log("Phone: " + valid);
+    return valid;
 }
