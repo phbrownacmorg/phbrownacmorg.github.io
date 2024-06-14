683eecca50ae8dff956ebcd683ab0f84914bc046
Peter Brown <phbrown@acm.org>
2024-06-14 00:59:47 -0400

    Setting up to demo AJAX

---
+++ AJAXdemo.js
@@ -0,0 +1,17 @@
+async function insertTimeText() {
+}
+
+async function insertTimeJson() {
+}
+
+async function insertWikipediaPage() {
+}
+
+async function getFlight() {
+}
+
+async function getLatLong() {
+}
+
+async function getWeather() {
+}
